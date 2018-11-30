3d69a6f27fefb5873c7c44ef5e0685720c35960d
manjunath makannavar <manjumaky@gmail.com>
2018-11-30 23:40:44 +0530

    first commit

---
+++ src/main/java/com/devops/MavenSonarDemo/App.java
@@ -0,0 +1,13 @@
+package com.devops.MavenSonarDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Maven Sonar Demo" );
+    }
+}
